727f2aca8d52889ede9968ad20520cb2d793cd11
Hyun-009 <iovemint@naver.com>
2025-04-02 12:15:18 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Toggle from './components/Toggle'
+import Ani from './components/Ani'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Ani />
+      <Toggle />
+    </div>
+  )
+}
+
+export default App
